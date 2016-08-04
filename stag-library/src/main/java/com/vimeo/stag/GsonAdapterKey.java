333d85a717938dd67c0be666ca0068df6e43c957
Anthony Restaino <AnthonyR@vimeo.com>
2016-08-04 11:12:37 -0400

    Clarified documentation

--- stag-library/src/main/java/com/vimeo/stag/GsonAdapterKey.java
+++ stag-library/src/main/java/com/vimeo/stag/GsonAdapterKey.java
@@ -31,21 +31,22 @@ import java.lang.annotation.Target;
  * or a class should be processed.
  * <p/>
  * If a member variable of a class is annotated, Stag will
- * generate a TypeAdapter for that class.
+ * generate a TypeAdapter for that class. Stag does not generate
+ * TypeAdapters for abstract classes.
  * <p/>
- * If the class is generic, but there is a concrete
- * class that also has annotated fields, Stag will ignore the
- * generic class and generate a TypeAdapter for the concrete
- * class that includes all the annotated member variables in
- * the inheritance hierarchy.
+ * If the class is generic (class A), but there is a concrete
+ * class (class B) that has annotated members, Stag will generate
+ * a TypeAdapter for the concrete class (class B) that also includes
+ * all the annotated member variables declared in class A.
+ * No adapter for class A will be generated.
  * <p/>
- * If a class inherits from a parent
- * class with annotated members, but has no annotated members
- * itself, Stag will not know to generate a TypeAdapter for that
- * child class. In that case, apply the annotation to the child
- * class and Stag will pick it up and generate a TypeAdapter
- * for it. In this case, the value that can be specified in the
- * annotation will just be ignored.
+ * If a class (class B) has no annotated members, but inherits from a
+ * parent class (class A) with annotated members, Stag will not know to
+ * generate a TypeAdapter for that child class (class B). In that case,
+ * apply the annotation to the child class declaration (class B) and
+ * then Stag will know to generate a TypeAdapter for it. In this case,
+ * the optional value that can be passed in the annotation will be
+ * ignored as it only applies to class fields, not classes themselves.
  * <p/>
  * See the sample code for examples of each.
  */
