49e66ac1012170c95540163e35fa6697cbb1597d
anirudh.r <anirudh.r@flipkart.com>
2016-12-09 02:32:58 +0530

    use serializedname

--- stag-library-compiler/src/main/java/com/vimeo/stag/processor/generators/AdapterGenerator.java
+++ stag-library-compiler/src/main/java/com/vimeo/stag/processor/generators/AdapterGenerator.java
@@ -1,8 +1,8 @@
 package com.vimeo.stag.processor.generators;
 
 
+import com.google.gson.annotations.SerializedName;
 import com.squareup.javapoet.TypeSpec;
-import com.vimeo.stag.GsonAdapterKey;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -11,12 +11,12 @@ import javax.lang.model.element.Element;
 public abstract class AdapterGenerator {
 
     /**
-     * If the element is not annotated with {@link GsonAdapterKey}, the variable name is used.
+     * If the element is not annotated with {@link SerializedName}, the variable name is used.
      */
     @NotNull
     protected static String getJsonName(@NotNull Element element) {
-        String name = (null != element.getAnnotation(GsonAdapterKey.class)) ?
-                element.getAnnotation(GsonAdapterKey.class).value() : null;
+        String name = (null != element.getAnnotation(SerializedName.class)) ?
+                element.getAnnotation(SerializedName.class).value() : null;
 
         if (null == name || name.isEmpty()) {
             name = element.getSimpleName().toString();
