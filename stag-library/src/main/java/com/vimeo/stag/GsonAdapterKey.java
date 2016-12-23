aa8d11712d450aed0e3ec4847daf85044c0e651e
anirudh.r <anirudh.r@flipkart.com>
2016-12-23 11:40:04 +0530

    Added @UseStag annotation, removed the support for class level for @GsonAdapterKey, and added some documentations

--- stag-library/src/main/java/com/vimeo/stag/GsonAdapterKey.java
+++ stag-library/src/main/java/com/vimeo/stag/GsonAdapterKey.java
@@ -27,30 +27,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
- * Use this annotation to tell Stag that a member variable
- * or a class should be processed.
- * <p/>
- * If a member variable of a class is annotated, Stag will
- * generate a TypeAdapter for that class. Stag does not generate
- * TypeAdapters for abstract classes.
- * <p/>
- * If the class is generic (class A), but there is a concrete
- * class (class B) that has annotated members, Stag will generate
- * a TypeAdapter for the concrete class (class B) that also includes
- * all the annotated member variables declared in class A.
- * No adapter for class A will be generated.
- * <p/>
- * If a class (class B) has no annotated members, but inherits from a
- * parent class (class A) with annotated members, Stag will not know to
- * generate a TypeAdapter for that child class (class B). In that case,
- * apply the annotation to the child class declaration (class B) and
- * then Stag will know to generate a TypeAdapter for it. In this case,
- * the optional value that can be passed in the annotation will be
- * ignored as it only applies to class fields, not classes themselves.
- * <p/>
- * See the sample code for examples of each.
+ * An annotation that indicates this member should be serialized to JSON with
+ * the provided name value as its field name.
  */
-@Target({ElementType.FIELD, ElementType.TYPE})
+@Target({ElementType.FIELD})
 public @interface GsonAdapterKey {
 
     /**
@@ -64,5 +44,4 @@ public @interface GsonAdapterKey {
      * or empty if there is none.
      */
     String value() default "";
-
 }
--- stag-library/src/main/java/com/vimeo/stag/UseStag.java
+++ stag-library/src/main/java/com/vimeo/stag/UseStag.java
@@ -27,42 +27,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
- * Use this annotation to tell Stag that a member variable
- * or a class should be processed.
+ * Use this annotation to tell Stag that all the member variables of the class should be processed.
  * <p/>
- * If a member variable of a class is annotated, Stag will
- * generate a TypeAdapter for that class. Stag does not generate
- * TypeAdapters for abstract classes.
+ * If the class is annotated, Stag will generate a TypeAdapter for that class.
+ * Stag does not generate TypeAdapters for abstract classes.
  * <p/>
- * If the class is generic (class A), but there is a concrete
- * class (class B) that has annotated members, Stag will generate
- * a TypeAdapter for the concrete class (class B) that also includes
- * all the annotated member variables declared in class A.
- * No adapter for class A will be generated.
- * <p/>
- * If a class (class B) has no annotated members, but inherits from a
- * parent class (class A) with annotated members, Stag will not know to
- * generate a TypeAdapter for that child class (class B). In that case,
- * apply the annotation to the child class declaration (class B) and
- * then Stag will know to generate a TypeAdapter for it. In this case,
- * the optional value that can be passed in the annotation will be
- * ignored as it only applies to class fields, not classes themselves.
- * <p/>
- * See the sample code for examples of each.
  */
 @Target({ElementType.TYPE})
 public @interface UseStag {
-
-    /**
-     * The optional value for the JSON key
-     * to which the member variable will be
-     * mapped. If none is supplied, then the
-     * name of the member variable will be
-     * used as the key.
-     *
-     * @return the value for the JSON key
-     * or empty if there is none.
-     */
-    String value() default "";
-
 }
