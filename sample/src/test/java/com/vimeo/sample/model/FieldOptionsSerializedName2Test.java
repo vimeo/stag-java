6ef363efb98410fcda21b1d0fa290db5f08123e0
Anthony Restaino <AnthonyR@vimeo.com>
2017-02-02 22:14:53 -0500

    Adding remaining unit tests for the model package

---
+++ sample/src/test/java/com/vimeo/sample/model/FieldOptionsSerializedName2Test.java
@@ -0,0 +1,17 @@
+package com.vimeo.sample.model;
+
+import com.vimeo.sample.Utils;
+
+import org.junit.Test;
+
+/**
+ * Created by restainoa on 2/2/17.
+ */
+public class FieldOptionsSerializedName2Test {
+
+    @Test
+    public void typeAdapterWasGenerated() throws Exception {
+        Utils.verifyTypeAdapterGeneration(FieldOptionsSerializedName2.class);
+    }
+
+}
