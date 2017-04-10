0e2a36f3d4100d8a589d5a9c4f73ce233cb6a5d7
anthony restaino <anthonyrestaino11@gmail.com>
2017-04-09 22:14:03 -0400

    Adding unit test to verify that a type adapter was created for new model

---
+++ sample-model/src/test/java/com/vimeo/sample_model/EnumWithFieldsModelTest.java
@@ -0,0 +1,15 @@
+package com.vimeo.sample_model;
+
+import org.junit.Test;
+
+/**
+ * Created by anthonycr on 4/9/17.
+ */
+public class EnumWithFieldsModelTest {
+
+    @Test
+    public void typeAdapterWasGenerated() throws Exception {
+        Utils.verifyTypeAdapterGeneration(EnumWithFieldsModel.class);
+    }
+
+}
