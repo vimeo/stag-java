5fddfce1441a0ad4cad22025d8ab1e098164c949
Anshul Garg <anshul.garg@flipkart.com>
2017-04-12 13:11:11 +0530

    added fix for object type adpater generation

---
+++ sample/src/test/java/com/vimeo/sample/model/ObjectExampleTest.java
@@ -0,0 +1,16 @@
+package com.vimeo.sample.model;
+
+import com.vimeo.sample.Utils;
+
+import org.junit.Test;
+
+/**
+ * Created by anshul.garg on 12/04/17.
+ */
+public class ObjectExampleTest {
+    @Test
+    public void typeAdapterWasGenerated() throws Exception {
+        Utils.verifyTypeAdapterGeneration(ObjectExample.class);
+    }
+
+}
