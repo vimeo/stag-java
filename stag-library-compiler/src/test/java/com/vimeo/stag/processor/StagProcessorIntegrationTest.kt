a9ef2afb0351e4fbd9d1f3f129fa577c5e5669a2
anirudhramanan <anirudh.r@flipkart.com>
2018-01-12 11:02:46 +0530

    fixed test cases

--- stag-library-compiler/src/test/java/com/vimeo/stag/processor/StagProcessorIntegrationTest.kt
+++ stag-library-compiler/src/test/java/com/vimeo/stag/processor/StagProcessorIntegrationTest.kt
@@ -2,10 +2,11 @@ package com.vimeo.stag.processor
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonWriter
 import com.vimeo.sample_java_model.NullFields
-import com.vimeo.sample_java_model.stag.generated.Stag
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.io.StringWriter
 
 /**
  * Integration tests for the compiler.
@@ -19,7 +20,11 @@ class StagProcessorIntegrationTest {
         val nullFields = NullFields()
         val typeToken = TypeToken.get(nullFields.javaClass)
         val typeAdapter = Stag.Factory().create(Gson(), typeToken)
-        val json = typeAdapter.toJson(nullFields)
+        val stringWriter = StringWriter()
+        val jsonWriter = JsonWriter(stringWriter)
+        jsonWriter.serializeNulls = false
+        typeAdapter.write(jsonWriter, nullFields)
+        val json = stringWriter.toString()
 
         // Assert that we are getting the JSON we expect
         assertThat(json).isEqualTo("{}")
