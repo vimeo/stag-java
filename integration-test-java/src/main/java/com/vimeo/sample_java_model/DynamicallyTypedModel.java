da40db250c26f59ebdfc082c069787344c9a4722
Anthony Restaino <AnthonyR@vimeo.com>
2018-01-17 15:15:33 -0500

    Adding documentation to wildcard models

--- integration-test-java/src/main/java/com/vimeo/sample_java_model/DynamicallyTypedModel.java
+++ integration-test-java/src/main/java/com/vimeo/sample_java_model/DynamicallyTypedModel.java
@@ -2,8 +2,14 @@ package com.vimeo.sample_java_model;
 
 import com.vimeo.stag.UseStag;
 
+/**
+ * A model with multiple generic types. Acting as a use case for {@link DynamicallyTypedWildcard}.
+ */
 @UseStag
 public class DynamicallyTypedModel<T extends BaseExternalModel, V extends ExternalModelGeneric<T>> {
+
     public T videoValue;
+
     public V dataListValue;
+
 }
--- integration-test-java/src/main/java/com/vimeo/sample_java_model/DynamicallyTypedWildcard.java
+++ integration-test-java/src/main/java/com/vimeo/sample_java_model/DynamicallyTypedWildcard.java
@@ -4,8 +4,14 @@ import com.vimeo.stag.UseStag;
 
 import java.util.List;
 
+/**
+ * A test case which tests a generic type that has wildcards. The type used is {@link DynamicallyTypedModel}.
+ */
 @UseStag
 public class DynamicallyTypedWildcard {
+
     public List<DynamicallyTypedModel<?, ?>> models;
+
     public String name;
+
 }
