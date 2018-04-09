913e9cdb83819d685a65946502527a497de0398f
Anthony Restaino <AnthonyR@vimeo.com>
2018-04-09 15:51:26 -0400

    Adding test case for Maps without types specified

--- integration-test-java/src/main/java/com/vimeo/sample_java_model/RawGenericField.java
+++ integration-test-java/src/main/java/com/vimeo/sample_java_model/RawGenericField.java
@@ -2,6 +2,8 @@ package com.vimeo.sample_java_model;
 
 import com.vimeo.stag.UseStag;
 
+import java.util.Map;
+
 /**
  * Model which references a generically typed object without generic bounds (i.e., as a raw
  * type).
@@ -11,5 +13,11 @@ public class RawGenericField {
 
     @SuppressWarnings("rawtypes")
     public ExternalModelGeneric rawTypedField;
+
+    /**
+     * Tests that map keys and values are handled correctly.
+     */
+    @SuppressWarnings("rawtypes")
+    public Map rawMap;
 
 }
