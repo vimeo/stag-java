c4ec29db9f2c0257a15a1d5ad027961dd57dbb25
Anthony Restaino <AnthonyR@vimeo.com>
2018-06-15 11:54:32 -0400

    Cleaning up Types class

--- stag-library/src/main/java/com/vimeo/stag/Types.java
+++ stag-library/src/main/java/com/vimeo/stag/Types.java
@@ -1,35 +1,41 @@
 package com.vimeo.stag;
 
 
+import org.jetbrains.annotations.NotNull;
+
 import java.lang.reflect.Type;
 import java.lang.reflect.WildcardType;
 
+@SuppressWarnings("unused")
 public final class Types {
 
     private Types() {}
 
-    public static WildcardType getWildcardType(Type[] upperBounds, Type[] lowerBounds) {
+    @NotNull
+    public static WildcardType getWildcardType(@NotNull Type[] upperBounds, @NotNull Type[] lowerBounds) {
         return new WildcardTypeImpl(upperBounds, lowerBounds);
     }
 
     private static final class WildcardTypeImpl implements WildcardType {
 
+        @NotNull
         private final Type[] upperBounds;
+        @NotNull
         private final Type[] lowerBounds;
 
-        public WildcardTypeImpl(Type[] upperBounds, Type[] lowerBounds) {
+        public WildcardTypeImpl(@NotNull Type[] upperBounds, @NotNull Type[] lowerBounds) {
             this.upperBounds = upperBounds;
             this.lowerBounds = lowerBounds;
         }
 
         @Override
         public Type[] getUpperBounds() {
-            return upperBounds;
+            return upperBounds.clone();
         }
 
         @Override
         public Type[] getLowerBounds() {
-            return lowerBounds;
+            return lowerBounds.clone();
         }
     }
 }
